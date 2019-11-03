c41933ee3b7ac48c378f2da9c88248c3efa73481
Vladimir <vladimir240298perm@gmail.com>
2019-11-03 19:13:04 +0300

    Begin to create some UI

--- пакеты/пакеты.py
+++ пакеты/пакеты.py
@@ -2,6 +2,11 @@ import difflib
 import os
 import subprocess
 import time
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication, QLabel, QLineEdit, QSlider)
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -57,5 +62,70 @@ def main():
     # os.system(program)
 
 
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        main_layout = QVBoxLayout()
+
+        test_scenario_label = QLabel("Test scenario path")
+        self.test_scenario_path = QLineEdit()
+
+        path_tests_label = QLabel("Path to file(folder) with test(tests)")
+        self.path_tests_path = QLineEdit()
+
+        self.test_params_label = QLabel("Params")
+
+        self.test_params_slider = QSlider(Qt.Horizontal, self)
+        self.test_params_slider.setFocusPolicy(Qt.StrongFocus)
+        self.test_params_slider.setTickPosition(QSlider.TicksBothSides)
+        self.test_params_slider.setTickInterval(1)
+        self.test_params_slider.setSingleStep(1)
+        self.test_params_slider.setMinimum(0)
+        self.test_params_slider.setMaximum(100)
+        self.test_params_slider.valueChanged[int].connect(self.on_slider_changed)
+
+        path_answers_label = QLabel("Path to default test answers")
+        self.path_answers_path = QLineEdit()
+
+        path_result_label = QLabel("Path to save answers folder")
+        self.path_result_path = QLineEdit()
+
+        path_comp_label = QLabel("Path to answers comparator file")
+        self.path_comp_path = QLineEdit()
+
+        calc_button = QPushButton("Calc")
+        calc_button.clicked.connect(self.on_calc_click)
+
+        main_layout.addWidget(test_scenario_label)
+        main_layout.addWidget(self.test_scenario_path)
+        main_layout.addWidget(path_tests_label)
+        main_layout.addWidget(self.path_tests_path)
+        main_layout.addWidget(self.test_params_label)
+        main_layout.addWidget(self.test_params_slider)
+        main_layout.addWidget(path_answers_label)
+        main_layout.addWidget(self.path_answers_path)
+        main_layout.addWidget(path_result_label)
+        main_layout.addWidget(self.path_result_path)
+        main_layout.addWidget(path_comp_label)
+        main_layout.addWidget(self.path_comp_path)
+        main_layout.addWidget(calc_button)
+
+        self.setLayout(main_layout)
+        self.setGeometry(300, 200, 1280, 720)
+        self.setWindowTitle('QLineEdit')
+
+    def on_slider_changed(self):
+        self.test_params_label.setText(self.test_params_slider.value())
+    def on_calc_click(self):
+        print(self.test_params_slider.value())
+
+
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+    # main()
